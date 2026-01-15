3fb775de80d6a742c045a68564cb42764421692d
Anil Hemnani <anil.hemnani@gmail.com>
2026-01-15 15:43:42 +0000

    Release version 2.0.4

--- src/distribution/bin/install-service.bat
+++ src/distribution/bin/install-service.bat
@@ -8,8 +8,8 @@ if %errorLevel% neq 0 (
 
 set "BIN_DIR=%~dp0"
 set APP_ROOT=%BIN_DIR%..
+sc stop WedKnots-Springboot
 C:\tools\nssm-2.24\win64\nssm.exe remove WedKnots-Springboot confirm
-C:\tools\nssm-2.24\win64\nssm.exe install "WedKnots-Springboot" "%BIN_DIR%start.bat" AppDirectory "%APP_ROOT%" Start SERVICE_AUTO_START
+C:\tools\nssm-2.24\win64\nssm.exe install "WedKnots-Springboot" "%BIN_DIR%start.bat" AppDirectory "%APP_ROOT%" Start SERVICE_AUTO_START ObjectName LocalSystem
 sc start WedKnots-Springboot
-nssm.exe set WedKnots-Springboot ObjectName LocalSystem
 pause
